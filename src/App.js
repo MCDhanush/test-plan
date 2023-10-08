5a74ab6095f9dda28f36bd5ff1d98f139f9bd160
Dhanush MC <dhanushmc93@gmail.com>
2023-10-08 14:21:23 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,180 @@
-import logo from './logo.svg';
 import './App.css';
+import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined';
+import EmailOutlinedIcon from '@mui/icons-material/EmailOutlined';
+import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
+import LocationOnOutlinedIcon from '@mui/icons-material/LocationOnOutlined';
+import LocalPhoneIcon from '@mui/icons-material/LocalPhone';
+import LockOpenOutlinedIcon from '@mui/icons-material/LockOpenOutlined';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+const [person, setPerson] = useState([])
+
+const persons = ()=>{
+  fetch("https://randomuser.me/api/")
+  .then((e)=>e.json())
+  // .then((e)=>console.log(e.results))
+  .then((person)=>setPerson(person.results))
+}
+useEffect(() => persons(), []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        person.map((m)=>(
+          <Header person={m}/>
+        ))
+      }
+    
+
     </div>
+   
   );
 }
+
+
+function Header({person}){
+
+  const [isHovering1, setIsHovering1] = useState(false);
+  const [isHovering2, setIsHovering2] = useState(false);
+  const [isHovering3, setIsHovering3] = useState(false);
+  const [isHovering4, setIsHovering4] = useState(false);
+  const [isHovering5, setIsHovering5] = useState(false);
+  const [isHovering6, setIsHovering6] = useState(false);
+
+  const handleMouseOver1 = () => {
+    setIsHovering1(true);
+  };
+
+  const handleMouseOut1 = () => {
+    setIsHovering1(false);
+  };
+  const handleMouseOver2 = () => {
+    setIsHovering2(true);
+  };
+
+  const handleMouseOut2 = () => {
+    setIsHovering2(false);
+  };
+  const handleMouseOver3 = () => {
+    setIsHovering3(true);
+  };
+
+  const handleMouseOut3 = () => {
+    setIsHovering3(false);
+  };
+  const handleMouseOver4 = () => {
+    setIsHovering4(true);
+  };
+
+  const handleMouseOut4 = () => {
+    setIsHovering4(false);
+  };
+  const handleMouseOver5 = () => {
+    setIsHovering5(true);
+  };
+
+  const handleMouseOut5 = () => {
+    setIsHovering5(false);
+  };
+  const handleMouseOver6 = () => {
+    setIsHovering6(true);
+  };
+
+  const handleMouseOut6 = () => {
+    setIsHovering6(false);
+  };
+  return(
+    <div className='box'>
+  <div className='body-box'>  
+   <div className='top-b'>
+    <span></span>
+   </div>
+  <div className='bottom-b'>
+    <img className='person-img' src={person.picture.large} />
+
+
+{/* span tags */}
+<div className='span'>
+<div>
+{isHovering1 && (
+          <div className='span-fi'> My,name is
+            <p  className='cnt-txt'>{person.name.first} {person.name.last} </p>
+            {/* <p>{person.name.last}</p> */}
+          </div>
+        )}
+</div>
+<div></div>
+   {isHovering2 && (
+          <div className='span-fi'>
+            My email is
+            <p className='cnt-txt'>{person.email}</p>
+          </div>
+        )}
+        <div></div>
+   {isHovering3 && (
+          <div className='span-fi'>
+            My dob is
+            <p  className='cnt-txt'>{person.dob.date}</p>
+          </div>
+        )}
+        
+        <div>
+   {isHovering4 && (
+          <div className='span-fi'>
+            My address is
+            <p  className='cnt-txt'>{person.location.street.number} {person.location.street.name}</p>
+          </div>
+        )}
+        </div>
+        <div>
+   {isHovering5 && (
+          <div className='span-fi'>
+            My phone number is
+            <p  className='cnt-txt'>{person.phone}</p>
+          </div>
+        )}
+        </div>
+        <div>
+   {isHovering6 && (
+          <div className='span-fi'> My password is
+            <p  className='cnt-txt'>{person.login.password}</p>
+          </div>
+        )}
+        </div>
+ </div>
+
+
+{/* ul list */}
+  <div>
+   <ul className='icons'>
+    <li onMouseOver={handleMouseOver1}
+          onMouseOut={handleMouseOut1}
+          className='icons-in'><PersonOutlineOutlinedIcon/>
+    </li>
+    <li onMouseOver={handleMouseOver2}
+          onMouseOut={handleMouseOut2} 
+          className='icons-in'><EmailOutlinedIcon/></li>
+    <li onMouseOver={handleMouseOver3}
+          onMouseOut={handleMouseOut3}  className='icons-in'><CalendarMonthIcon/></li>
+
+    <li onMouseOver={handleMouseOver4}
+          onMouseOut={handleMouseOut4} className='icons-in'><LocationOnOutlinedIcon/></li>
+
+    <li onMouseOver={handleMouseOver5}
+          onMouseOut={handleMouseOut5} className='icons-in'><LocalPhoneIcon/></li>
+
+    <li onMouseOver={handleMouseOver6}
+          onMouseOut={handleMouseOut6} className='icons-in'><LockOpenOutlinedIcon/></li>
+   </ul>
+   </div>
+
+   </div>
+  </div>
+  </div>
+
+  )
+}
+
 
 export default App;
---
+++ src/personlist.js
@@ -0,0 +1,66 @@
+// {
+//     results: [
+//       {
+//         gender: "male",
+//         name: {
+//           title: "Mr",
+//           first: "Scott",
+//           last: "Ward"
+//         },
+//         location: {
+//           street: {
+//             number: 5888,
+//             name: "Green Rd"
+//           },
+//           city: "Scottsdale",
+//           state: "Idaho",
+//           country: "United States",
+//           postcode: 81474,
+//           coordinates: {
+//             latitude: "85.8166",
+//             longitude: "-48.8990"
+//           },
+//           timezone: {
+//             offset: "-6:00",
+//             description: "Central Time (US & Canada), Mexico City"
+//           }
+//         },
+//         email: "scott.ward@example.com",
+//         login: {
+//           uuid: "de394c3c-2377-42b3-bf67-b84e4ba6b203",
+//           username: "greenkoala752",
+//           password: "werewolf",
+//           salt: "loDDlRlR",
+//           md5: "1fb1298b2a78da00a47dcfebd44c5605",
+//           sha1: "c81ba84397eec207d7b5f1c9c34e909b82d5a9f6",
+//           sha256: "6c6397ea2a2da59d986bf3494c6f247630f5769aa10e7c166979511969aee0ef"
+//         },
+//         dob: {
+//           date: "1975-12-11T02:50:59.376Z",
+//           age: 47
+//         },
+//         registered: {
+//           date: "2007-04-25T07:00:05.755Z",
+//           age: 16
+//         },
+//         phone: "(476) 571-0267",
+//         cell: "(794) 698-7655",
+//         id: {
+//           name: "SSN",
+//           value: "084-76-0503"
+//         },
+//         picture: {
+//           large: "https://randomuser.me/api/portraits/men/90.jpg",
+//           medium: "https://randomuser.me/api/portraits/med/men/90.jpg",
+//           thumbnail: "https://randomuser.me/api/portraits/thumb/men/90.jpg"
+//         },
+//         nat: "US"
+//       }
+//     ],
+//     info: {
+//       seed: "7463dcb63f815632",
+//       results: 1,
+//       page: 1,
+//       version: "1.4"
+//     }
+//   }
